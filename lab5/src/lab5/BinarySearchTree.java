5b05a4c14507bbbd7f28ff02190250c3fd32266c
Issao Fujiwara <issaofujiwara@gmail.com>
2017-06-13 23:33:03 -0700

    Initial starter code.

---
+++ lab5/src/lab5/BinarySearchTree.java
@@ -0,0 +1,104 @@
+package lab5;
+
+public class BinarySearchTree {
+
+    private class Node {
+	// The value stored in this tree node.
+	private int value;
+
+	// The left child of this node. Can be null if there is none.
+	private Node leftChild;
+
+	// The right child of this node. Can be null if there is none.
+	private Node rightChild;
+
+	// Create a node with given value and no children.
+	private Node(int value) {
+	    this.value = value;
+	    this.leftChild = null;
+	    this.rightChild = null;
+	}
+
+	private int getValue() {
+	    return this.value;
+	}
+
+	private void setValue(int value) {
+	    this.value = value;
+	}
+
+	private Node getLeftChild() {
+	    return this.leftChild;
+	}
+
+	private Node getRightChild() {
+	    return this.rightChild;
+	}
+
+	private void setLeftChild(Node child) {
+	    this.leftChild = child;
+	}
+
+	private void setRightChild(Node child) {
+	    this.rightChild = child;
+	}
+
+	private void insertNode(Node node) {
+	    if (node.value < this.value) {
+		// Node value is less than this, so insert
+		// the node in the left subtree.
+		if (this.leftChild == null) {
+		    // No left child, so make node left child.
+		    this.leftChild = node;
+		} else {
+		    // Recurse into left subtree.
+		    this.leftChild.insertNode(node);
+		}
+	    } else if (node.value > this.value) {
+		// Node value is greater than this, so insert
+		// the node in the right subtree.
+		if (this.rightChild == null) {
+		    // No right child, so make node right child.
+		    this.rightChild = node;
+		} else {
+		    // Recurse into right subtree.
+		    this.rightChild.insertNode(node);
+		}
+	    }
+	}
+    }
+
+    private Node root;
+
+    // Constructor for an empty tree.
+    public BinarySearchTree() {
+	this.root = null;
+    }
+
+    // Inserts the given value in this tree.
+    // Inserting a value more than once is treated as a no-op.
+    public void insert(int value) {
+	Node newNode = new Node(value);
+	if (this.root == null) {
+	    // BinarySearchTree empty, so set the new node as root node.
+	    this.root = newNode;
+	} else {
+	    this.root.insertNode(newNode);
+	}
+    }
+
+    // Checks if this BinarySearchTree contains the given value.
+    // Returns true iff the value is present in this BinarySearchTree.
+    public boolean find(int value) {
+	// TODO: You should implement find() here.
+	// Note that you will likely want to implement a 
+	return false;
+    }
+
+    // Returns the count of entries in this BinarySearchTree that are
+    // less than value.
+    public int countLessThan(int value) {
+	// TODO: You should implement countLessThan() here.
+	return 0;
+    }
+}
---
+++ lab5/src/lab5/DataCenterOrders.java
@@ -0,0 +1,34 @@
+package lab5;
+
+import java.util.Scanner;
+
+public class DataCenterOrders {
+    public static void main(String[] args) {
+	Scanner in = new Scanner(System.in);
+	int numOperations = in.nextInt();
+
+	// TODO: Define any data structures that you will need here.
+
+	for (int i = 0; i < numOperations; ++i) {
+	    String op = in.next();
+	    if (op.contentEquals("Insert")) {
+		int dataCenterSize = in.nextInt();
+
+		// TODO: Insert dataCenterSize into your data structure.
+
+	    } else if (op.contentEquals("Rangex")) {
+		int min = in.nextInt();
+		int max = in.nextInt();
+		int count = 0;
+
+		// TODO: Set count to the number of data center of sizes
+		// greater than or equal to min, but less than max.
+
+		System.out.println(count);
+	    } else {
+		System.err.println("Unknown operation: " + op);
+		System.exit(1);
+	    }
+	}
+    }
+}
